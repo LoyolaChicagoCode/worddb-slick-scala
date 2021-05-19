720cab03e3605badb7e8f2f84f667505993964ad
Konstantin Läufer <klaeufer@gmail.com>
2021-05-19 13:57:51 -0500

    simplified main method
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,6 @@
-import DAO.Row
 import caseapp._
 
+import java.text.MessageFormat
 import java.util.{ Locale, ResourceBundle }
 import scala.util.{ Try, Using }
 
@@ -55,41 +55,49 @@ object Main extends CaseApp[Options] {
     // WONTFIX figure out how to go from a positional to a named representation of actual options
     // val optionsMap = options.productElementNames.zip(options.productIterator).filter { case (_, None) => false case _ => true }.toMap
 
-    /** Externalized resource bundle in src/main/resources. */
+    // Externalized resource bundle in src/main/resources.
     val bundle = ResourceBundle.getBundle("messages", Locale.US)
 
-    /** Prints externalized message for given key. */
-    def p[R](key: String): R => Unit = _ => println(bundle.getString(key))
-
-    /** Invokes DAO command and, if successful, prints message for given key. */
-    def c[R](command: DAO => Try[R], key: String): Unit = Using(new DAOImpl(dbPath)) { dao =>
-      command(dao).fold(p("failed"), _ => p(key))
+    def injectDAO(action: DAO => Try[Unit]): Unit = Using.resource(new DAOImpl(dbPath)) { dao =>
+      action(dao).fold(_ => printMessageFormat("failed"), _ => ())
     }
 
-    /** Invokes DAO command and, if successful, prints resulting word counts. */
-    def q(command: DAO => Try[Seq[Row]]): Unit = Using(new DAOImpl(dbPath)) { dao =>
-      command(dao).map(rows => rows.foreach(row => println(row._1 + " -> " + row._2)))
+    def printMessageFormat(key: String, args: Any*): Unit = {
+      val form = new MessageFormat(bundle.getString(key))
+      println(form.format(args.toArray))
     }
 
     options match {
       case Options(_, false, false, None, None, None, None, None) =>
-        p("noCommand")
-      case Options(_, true, false, None, None, None, None, None) =>
-        c(_.createDatabase(), "created")
-      case Options(_, false, true, None, None, None, None, None) =>
-        q(_.showWordCounts())
-      case Options(_, false, false, Some(word), None, None, None, None) =>
-        c(_.addWord(word), "added")
-      case Options(_, false, false, None, Some(word), None, None, None) =>
-        c(_.deleteWord(word), "deleted")
-      case Options(_, false, false, None, None, Some(word), None, None) =>
-        c(_.incWordCount(word), "incremented")
-      case Options(_, false, false, None, None, None, Some(word), None) =>
-        c(_.decWordCount(word), "decremented")
-      case Options(_, false, false, None, None, None, None, Some(text)) =>
-        q(_.findInWords(text))
+        printMessageFormat("noCommand")
+      case Options(_, true, false, None, None, None, None, None) => injectDAO { dao =>
+        dao.createDatabase().map(_ => printMessageFormat("created"))
+      }
+      case Options(_, false, true, None, None, None, None, None) => injectDAO { dao =>
+        dao.showWordCounts().map {
+          case Seq() => printMessageFormat("noWordCounts")
+          case rows => rows.foreach(row => println(row._1 + " -> " + row._2))
+        }
+      }
+      case Options(_, false, false, Some(word), None, None, None, None) => injectDAO { dao =>
+        dao.addWord(word).map(_ => printMessageFormat("added", word))
+      }
+      case Options(_, false, false, None, Some(word), None, None, None) => injectDAO { dao =>
+        dao.deleteWord(word).map(_ => printMessageFormat("deleted", word))
+      }
+      case Options(_, false, false, None, None, Some(word), None, None) => injectDAO { dao =>
+        dao.incWordCount(word).map(count => printMessageFormat("incremented", word, count))
+      }
+      case Options(_, false, false, None, None, None, Some(word), None) => injectDAO { dao =>
+        dao.decWordCount(word).map {
+          case 0 => printMessageFormat("deleted", word)
+          case count => printMessageFormat("decremented", word, count)
+        }
+      }
+      case Options(_, false, false, None, None, None, None, Some(text)) => ???
+      //q(_.findInWords(text))
       case _ =>
-        p("multipleCommands")
+        printMessageFormat("multipleCommands")
     }
   }
 }
