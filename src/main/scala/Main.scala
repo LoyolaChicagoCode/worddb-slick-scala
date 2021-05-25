724df7f7268e15aa15417be2eede7f65ec246c59
Konstantin Läufer <klaeufer@gmail.com>
2021-05-25 18:42:00 -0500

    updated inline comments explaining alternative coding styles
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,8 @@ import java.text.MessageFormat
 import java.util.{ Locale, ResourceBundle }
 import scala.util.{ Try, Using }
 
+// DONE for each for-comprehension, add inline comment indicating corresponding command-line option
+
 object Main extends App {
 
   private val logger = org.log4s.getLogger
@@ -60,23 +62,20 @@ object Main extends App {
   numOptions match {
     case 0 => printMessageFormat("noCommand")
     case 1 =>
-
-      // TODO for each for-comprehension, add inline comment indicating corresponding
-      // command-line option
-
+      // --create-database/-f
       for {
         true <- OptionT.fromOption[Try](options.createDatabase.toOption)
         () <- withDAO { dao => dao.createDatabase() }
       } yield printMessageFormat("created")
 
-      // TODO compare above with below
-
+      // alternative to above using Try without OptionT:
       //      for {
       //        () <- Try(())
       //        if options.createDatabase.isDefined
       //        () <- withDAO { dao => dao.createDatabase() }
       //      } yield printMessageFormat("created")
 
+      // --show-word-counts/-s
       for {
         true <- OptionT.fromOption[Try](options.showWordCounts.toOption)
         counts <- withDAO { dao => dao.showWordCounts() }
@@ -85,26 +84,27 @@ object Main extends App {
         case rows => rows.foreach(row => println(row._1 + " -> " + row._2))
       }
 
-      // before introducing OptionT: deep nesting
-      //      options.addWord.foreach { word =>
-      //        injectDAO { dao =>
-      //          dao.addWord(word).map(_ => printMessageFormat("added", word))
-      //        }
-      //      }
-
-      // with OptionT: sequential - can mix Try and Option sequentially without nesting
+      // --add-word/-a word
+      // with for-comprehension *and* OptionT: can mix Try and Option sequentially without nesting
       for {
         word <- OptionT.fromOption[Try](options.addWord)
         () <- withDAO { dao => dao.addWord(word) }
       } yield printMessageFormat("added", word)
 
+      // alternative to above without for-comprehension: deep nesting
+      //      options.addWord.foreach { word =>
+      //        withDAO { dao =>
+      //          dao.addWord(word).map(_ => printMessageFormat("added", word))
+      //        }
+      //      }
+
+      // --delete-word/-x word
       for {
         word <- OptionT.fromOption[Try](options.deleteWord)
         () <- withDAO { dao => dao.deleteWord(word) }
       } yield printMessageFormat("deleted", word)
 
-      // TODO compare above with below
-
+      // alternative to above without OptionT:
       //      for {
       //        () <- Try(())
       //        if options.deleteWord.isDefined
@@ -112,11 +112,13 @@ object Main extends App {
       //        () <- withDAO { dao => dao.deleteWord(word) } // requires simplified return type for withDAO
       //      } yield printMessageFormat("deleted", word)
 
+      // --inc-word-count/-i word
       for {
         word <- OptionT.fromOption[Try](options.incWordCount)
         count <- withDAO { dao => dao.incWordCount(word) }
       } yield printMessageFormat("incremented", word, count)
 
+      // --dec-word-count/-d word
       for {
         word <- OptionT.fromOption[Try](options.decWordCount)
         count <- withDAO { dao => dao.decWordCount(word) }
@@ -125,6 +127,7 @@ object Main extends App {
         case count => printMessageFormat("decremented", word, count)
       }
 
+      // --find-in-words/--w word
       for {
         _ <- options.findInWords
       } yield printMessageFormat("nyi")
